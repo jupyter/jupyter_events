1b1627b81267edbf8c7dc0eff1722836f34e10bc
Nicholas Bollweg <nick.bollweg@gmail.com>
2022-09-09 16:22:20 -0500

    refactor cli, normalize paths

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,5 @@
+import pytest
+
 import jupyter_events
 from jupyter_events.cli import RC
 
@@ -7,42 +9,50 @@ NAME = "jupyter-events"
 VALIDATE = NAME, "validate"
 
 
-def test_cli_version(script_runner):
-    ret = script_runner.run(NAME, "--version")
+@pytest.fixture
+def cli(script_runner):
+    def run_cli(*args, **kwargs):
+        return script_runner.run(NAME, *map(str, args), **kwargs)
+
+    return run_cli
+
+
+def test_cli_version(cli):
+    ret = cli("--version")
     assert ret.success
     assert ret.stdout.strip() == f"{NAME}, version {jupyter_events.__version__}"
 
 
-def test_cli_help(script_runner):
-    ret = script_runner.run(NAME, "--help")
+def test_cli_help(cli):
+    ret = cli("--help")
     assert ret.success
     assert f"Usage: {NAME}" in ret.stdout.strip()
 
 
-def test_cli_good(script_runner):
+def test_cli_good(cli):
     """jupyter events validate path/to/my_schema.json"""
-    ret = script_runner.run(*VALIDATE, SCHEMA_PATH / "good/array.yaml")
+    ret = cli("validate", SCHEMA_PATH / "good/array.yaml")
     assert ret.success
     assert not ret.stderr.strip()
     assert "This schema is valid" in ret.stdout
 
 
-def test_cli_missing(script_runner):
-    ret = script_runner.run(*VALIDATE, SCHEMA_PATH / "bad/doesnt-exist.yaml")
+def test_cli_missing(cli):
+    ret = cli("validate", SCHEMA_PATH / "bad/doesnt-exist.yaml")
     assert not ret.success
     assert ret.returncode == RC.UNPARSEABLE
     assert "Schema file not present" in ret.stderr.strip()
 
 
-def test_cli_malformed(script_runner):
-    ret = script_runner.run(*VALIDATE, SCHEMA_PATH / "bad/invalid.yaml")
+def test_cli_malformed(cli):
+    ret = cli("validate", SCHEMA_PATH / "bad/invalid.yaml")
     assert not ret.success
     assert ret.returncode == RC.UNPARSEABLE
     assert "Could not deserialize" in ret.stderr.strip()
 
 
-def test_cli_invalid(script_runner):
-    ret = script_runner.run(*VALIDATE, SCHEMA_PATH / "bad/reserved-property.yaml")
+def test_cli_invalid(cli):
+    ret = cli("validate", SCHEMA_PATH / "bad/reserved-property.yaml")
     assert not ret.success
     assert ret.returncode == RC.INVALID
     assert "The schema failed to validate" in ret.stderr.strip()
