de4b4d6100c79206d5a2d70f101f4f20bbdd6311
Steven Silvester <steven.silvester@ieee.org>
2022-10-16 20:49:14 -0500

    [ci skip] rename workflow

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,13 @@ source_suffix = [".rst", ".md"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "_contents", "Thumbs.db", ".DS_Store", "demo/demo-notebook.ipynb"]
+exclude_patterns = [
+    "_build",
+    "_contents",
+    "Thumbs.db",
+    ".DS_Store",
+    "demo/demo-notebook.ipynb",
+]
 
 
 # -- Options for HTML output -------------------------------------------------
