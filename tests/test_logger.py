192c83f1a331dc9b38af5bf44901471ecd60e445
bretttully <brett.tully@nearmap.com>
2023-01-05 09:00:45 +1100

    [temp commit] test for error

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -258,6 +258,7 @@ def test_emit_badschema_format():
     el = EventLogger(handlers=[logging.NullHandler()])
     el.register_event_schema(schema)
 
+    el.emit(schema_id="http://test/test", data={"something": "chucknorris"})
     with pytest.raises(jsonschema.ValidationError) as excinfo:
         el.emit(schema_id="http://test/test", data={"something": "chucknorris"})
 
