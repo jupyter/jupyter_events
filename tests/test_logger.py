cd9a5edfa29d65c854628377df0291591d1d0bc7
Steven Silvester <steven.silvester@ieee.org>
2023-07-28 22:21:54 -0500

    more test fixes

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -206,13 +206,16 @@ def test_message_field():
     assert "__timestamp__" in event_capsule
     # Remove timestamp from capsule when checking equality, since it is gonna vary
     del event_capsule["__timestamp__"]
-    assert event_capsule == {
+    expected = {
         "__schema__": "http://test/test",
         "__schema_version__": 1,
         "__metadata_version__": 1,
         "something": "blah",
         "message": "a message was seen",
     }
+    if sys.version_info >= (3, 12):
+        expected["taskName"] = None
+    assert event_capsule == expected
 
 
 def test_nested_message_field():
@@ -253,12 +256,15 @@ def test_nested_message_field():
     assert "__timestamp__" in event_capsule
     # Remove timestamp from capsule when checking equality, since it is gonna vary
     del event_capsule["__timestamp__"]
-    assert event_capsule == {
+    expected = {
         "__schema__": "http://test/test",
         "__schema_version__": 1,
         "__metadata_version__": 1,
         "thing": {"message": "a nested message was seen"},
     }
+    if sys.version_info >= (3, 12):
+        expected["taskName"] = None
+    assert event_capsule == expected
 
 
 def test_register_event_schema(tmp_path):
