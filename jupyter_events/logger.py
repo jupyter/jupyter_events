3eba67325f7a118f9491835464fb63a7f2eb8824
Steven Silvester <steven.silvester@ieee.org>
2023-10-29 15:11:57 -0500

    fix type

--- jupyter_events/logger.py
+++ jupyter_events/logger.py
@@ -121,8 +121,8 @@ class EventLogger(LoggingConfigurable):
         self,
         cfg: Config,
         section_names: list[str] | None = None,
-        traits: list[str] | None = None,
-    ) -> None:  # type:ignore[override]
+        traits: list[str] | None = None,  # type:ignore[override]
+    ) -> None:
         """Load EventLogger traits from a Config object, patching the
         handlers trait in the Config object to avoid deepcopy errors.
         """
